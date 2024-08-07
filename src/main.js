c1987c7d4b5920f36387619b6d59439ecbd98dc6
Ethan <ethanhud6@gmail.com>
2024-08-07 15:24:18 +0100

    Updated devserver, added html and queryselections

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import Ship from './ship.js';
 import GameBoard from './gameboard.js';
 import Player from './player.js';
 import AI from './AI.js';
+
+// Query Selection
+const playerBoardDOM = document.querySelector('.player-board');
+const computerBoardDOM = document.querySelector('.computer-board');
+const playerShipsDOM = document.querySelector('.player-ships');
+
+// Player Ships
+// Enemy Ships
+// Board
+let playerBoard = new GameBoard();
+let enemyBoard = new GameBoard();
+// Players
+let player = new Player('Player 1');
+let computer = new AI('Player 2', player, enemyBoard);

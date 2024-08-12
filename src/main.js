a4bc0d941e114d4c6b276c0f3c1ef2f691a0061d
Ethan <ethanhud6@gmail.com>
2024-08-12 15:34:35 +0100

    added board generation

--- src/main.js
+++ src/main.js
@@ -2,18 +2,34 @@ import './styles/style.scss';
 import Ship from './ship.js';
 import GameBoard from './gameboard.js';
 import Player from './player.js';
-import AI from './AI.js';
+import Computer from './AI.js';
 
 // Query Selection
 const playerBoardDOM = document.querySelector('.player-board');
 const computerBoardDOM = document.querySelector('.computer-board');
 const playerShipsDOM = document.querySelector('.player-ships');
 
-// Player Ships
-// Enemy Ships
-// Board
-let playerBoard = new GameBoard();
-let enemyBoard = new GameBoard();
-// Players
-let player = new Player('Player 1');
-let computer = new AI('Player 2', player, enemyBoard);
+document.addEventListener('DOMContentLoaded', () => {
+	let playerBoard = new GameBoard();
+	let enemyBoard = new GameBoard();
+	let player = new Player('Player 1');
+	let computer = new Computer('Player 2', player, playerBoard);
+
+	displayBoard('player-board');
+	displayBoard('computer-board');
+});
+
+function displayBoard(boardName) {
+	let board = document.querySelector(`.${boardName}`);
+
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			let cell = document.createElement('div');
+			cell.classList.add('cell');
+			cell.setAttribute('data-x', i);
+			cell.setAttribute('data-y', j);
+
+			board.appendChild(cell);
+		}
+	}
+}
